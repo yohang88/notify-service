7e16c7c99273186171a0106328ff45897a87ed5e
Yoga Hanggara <yohang88@gmail.com>
2020-02-01 12:33:31 +0700

    http endpoint for creating broadcast

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+    "log"
+    "net/http"
     "strconv"
     "time"
+
+    "github.com/labstack/echo"
 
     "github.com/sirupsen/logrus"
     "github.com/yohang88/notify-service/queue"
@@ -12,8 +16,30 @@ func main() {
     logrus.SetFormatter(&logrus.JSONFormatter{})
 
     queue.Init("amqp://localhost")
+    // publisher()
 
-    publisher()
+    e := echo.New()
+    // e.Use(middleware.Logger())
+
+    e.GET("/", func(c echo.Context) error {
+        return c.JSON(http.StatusOK, map[string]string{"version": "1.0.0"})
+    })
+
+    e.POST("/broadcasts", createBroadcast)
+
+    e.Logger.Fatal(e.Start(":8000"))
+}
+
+func createBroadcast(c echo.Context) error {
+    err := queue.Publish("push_message", []byte(`{"num":6.13,"data":["a","b"]}`))
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    logrus.WithFields(logrus.Fields{"event_name": "BROADCAST_CREATE"}).Info("Create broadcast")
+
+    return c.NoContent(http.StatusNoContent)
 }
 
 func publisher() {
